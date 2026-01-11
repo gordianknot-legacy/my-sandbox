22b20003f8c7f5343edc2a9c079f18445c12cb33
Anirudh <g.anirudh.sharma@gmail.com>
2026-01-11 18:52:42 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -38,13 +38,8 @@ export const metadata: Metadata = {
     apple: [
       { url: "/apple-touch-icon.png", sizes: "180x180", type: "image/png" },
     ],
-    other: [
-      { rel: "icon", url: "/android-chrome-192x192.png", sizes: "192x192", type: "image/png" },
-      { rel: "icon", url: "/android-chrome-512x512.png", sizes: "512x512", type: "image/png" },
-    ],
   },
   manifest: "/site.webmanifest",
-  metadataBase: new URL("https://whybe.ai"),
 };
 
 export default function RootLayout({
